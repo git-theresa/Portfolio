0d9a4a1503dfaa245a0ff20c4ee8f9c12f3f6cc0
Theresa E <t.eatherly@gmail.com>
2020-06-20 11:57:00 -0500

    initial files created

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/pages/Home";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/portfolio" component={Portfolio} />
+             <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
