4f0652c68cf785d0e4d3acabfb4b53129fe89e8c
Theresa E <t.eatherly@gmail.com>
2020-06-26 09:55:50 -0500

    card link functioning

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function NavTabs() {
-  // We'll go into the Hooks API later, for now, we are just using some code
-  // from the react-router docs (https://reacttraining.com/react-router/web/api/Hooks/uselocation)
-  // This allows the component to check the route any time the user uses a link to navigate.
-  const location = useLocation();
+    const location = useLocation();
 
   return (
+
     <ul className="nav nav-tabs">
       <li className="nav-item">
         <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
@@ -43,3 +41,11 @@ function NavTabs() {
 }
 
 export default NavTabs;
+//     <Nav defaultActiveKey="/home" className="flex-column">
+//   <Nav.Link href="/home">Active</Nav.Link>
+//   <Nav.Link eventKey="link-1">Link</Nav.Link>
+//   <Nav.Link eventKey="link-2">Link</Nav.Link>
+//   <Nav.Link eventKey="disabled" disabled>
+//     Disabled
+//   </Nav.Link>
+// </Nav>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,22 +1,123 @@
 import React from "react";
+import {Card, CardColumns, Button} from "react-bootstrap";
+import {Image} from "./TE.jpg";
+
+const styles = {
+buttonContainer: {
+  display: "flex", 
+  justifyContent: "space-between"
+},
+button: {
+  width: "30px !important" 
+} 
+}
+
 
 function Portfolio() {
   return (
-    <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula. Curabitur pellentesque
-        turpis sit amet eros iaculis, a mollis arcu dictum. Ut vel ante eget massa ornare placerat.
-        Etiam nisl orci, finibus sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum
-        nunc, sed pretium risus rutrum eget. Nam consequat, ligula in faucibus vestibulum, nisi
-        justo laoreet risus, luctus luctus mi lacus sit amet libero. Class aptent taciti sociosqu ad
-        litora torquent per conubia nostra, per inceptos himenaeos. Mauris pretium condimentum
-        tellus eget lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec
-        placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum in velit pellentesque
-        vehicula sit amet at augue. Maecenas aliquam bibendum congue. Pellentesque semper, lectus
-        non ullamcorper iaculis, est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+    <CardColumns>
+      <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={Image} />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+  </Card.Body>
+  
+  <Card.Body>
+    <div style={styles.buttonContainer}>
+  <Button size="sm" variant="primary" href="https://git-theresa.github.io/WeatherDashboard/" target="_blank">Weather Dashboard</Button>
+  <Button size="sm" variant="primary" href="https://github.com/git-theresa/WeatherDashboard" target="_blank">See Code</Button>
     </div>
+  </Card.Body>
+</Card>
+  {/* <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title that wraps to a new line</Card.Title>
+      <Card.Text>
+        This is a longer card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+
+  <Card className="p-3">
+    <blockquote className="blockquote mb-0 card-body">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+        erat a ante.
+      </p>
+      <footer className="blockquote-footer">
+        <small className="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </Card>
+
+  <Card>
+    <Card.Img src="holder.js/100px160" />
+  </Card>
+
+  <Card bg="primary" text="white" className="text-center p-3">
+    <blockquote className="blockquote mb-0 card-body">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+        erat a ante.
+      </p>
+      <footer className="blockquote-footer">
+        <small className="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </Card>
+
+  <Card className="text-center">
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+      <Card.Text>
+        <small className="text-muted">Last updated 3 mins ago</small>
+      </Card.Text>
+    </Card.Body>
+  </Card>
+
+  <Card className="text-right">
+    <blockquote className="blockquote mb-0 card-body">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+        erat a ante.
+      </p>
+      <footer className="blockquote-footer">
+        <small className="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </Card>
+
+  <Card>
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+      <Card.Text>
+        <small className="text-muted">Last updated 3 mins ago</small>
+      </Card.Text>
+    </Card.Body>
+  </Card> */}
+</CardColumns>
+ 
   );
 }
 
