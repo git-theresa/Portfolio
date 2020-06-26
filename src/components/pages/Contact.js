454001ac7b009126d5037aab8c834dca51925a92
Theresa E <t.eatherly@gmail.com>
2020-06-26 17:50:42 -0500

    Contact page cleaned

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -11,22 +11,22 @@ const styles = {
 
 function Contact(props) {
   return (
-    // <div className="container" style={styles.Image}>
-    //   <h1>Contact Page</h1>
-    //   <img style={styles.Image} src={Image} alt="profile" />
-    //     <p>
-    //       <i class="fa fa-map-marker fa-fw w3-text-white w3-xxlarge w3-margin-right"></i>
-    //       Nashville, TN
-    //     </p>
-    //     <p>
-    //       <i class="fa fa-phone fa-fw w3-text-white w3-xxlarge w3-margin-right"></i>{" "}
-    //       Phone: 615.310.1384
-    //     </p>
-    //     <p>
-    //       <i class="fa fa-envelope fa-fw w3-text-white w3-xxlarge w3-margin-right"></i>{" "}
-    //       Email: T.eatherly@gmail.com
-    //     </p>
-    //   </div>
+    <div className="container" style={styles.Image}>
+      <h1>Contact Page</h1>
+      <img style={styles.Image} src={Image} alt="profile" />
+        <p>
+          <i class="fa fa-map-marker fa-fw w3-text-white w3-xxlarge w3-margin-right"></i>
+          Nashville, TN
+        </p>
+        <p>
+          <i class="fa fa-phone fa-fw w3-text-white w3-xxlarge w3-margin-right"></i>{" "}
+          Phone: 615.310.1384
+        </p>
+        <p>
+          <i class="fa fa-envelope fa-fw w3-text-white w3-xxlarge w3-margin-right"></i>{" "}
+          Email: T.eatherly@gmail.com
+        </p>
+      </div>
   );
 }
 
